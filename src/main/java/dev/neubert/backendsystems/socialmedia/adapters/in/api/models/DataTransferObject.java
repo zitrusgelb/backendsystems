b5dc335efff399ca2cca2e503a7077366e486cfa
Laurin <laurin.neubert@ln13.de>
2024-12-13 19:26:08 +0100

    feat: created abstract classes and interfaces

---
+++ src/main/java/dev/neubert/backendsystems/socialmedia/adapters/in/api/models/DataTransferObject.java
@@ -0,0 +1,5 @@
+package dev.neubert.backendsystems.socialmedia.adapters.in.api.models;
+
+public interface DataTransferObject {
+    String toString();
+}
---
+++ src/main/java/dev/neubert/backendsystems/socialmedia/application/domain/models/Model.java
@@ -0,0 +1,5 @@
+package dev.neubert.backendsystems.socialmedia.application.domain.models;
+
+public interface Model {
+    String toString();
+}
