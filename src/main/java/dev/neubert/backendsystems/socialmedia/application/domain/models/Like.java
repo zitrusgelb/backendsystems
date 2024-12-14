975e8989680d2c60dddf9c84ce7bc3c14a13879c
Tim <tim.knuettel@study.thws.de>
2024-12-14 15:47:07 +0100

    feat: Like.java, LikeDto.java, LikeEntity.java, LikeEntityId.java

---
+++ src/main/java/dev/neubert/backendsystems/socialmedia/application/domain/models/Like.java
@@ -0,0 +1,47 @@
+package dev.neubert.backendsystems.socialmedia.application.domain.models;
+
+import java.time.LocalDateTime;
+
+public class Like extends AbstractModel
+{
+    private Post post;
+   // private User user;
+    private LocalDateTime timestamp;
+
+    public Like(Post post)
+    {
+        this.post = post;
+        //this.user = user;
+        timestamp = LocalDateTime.now();
+    }
+
+    public Post getPost()
+    {
+        return post;
+    }
+
+    public void setPost(Post post)
+    {
+        this.post = post;
+    }
+/*public User getUser()
+    {
+        return user;
+    }
+
+    public void setUser(User user)
+    {
+        this.user=user;
+    }
+ */
+
+    public LocalDateTime getTimestamp()
+    {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp)
+    {
+        this.timestamp = timestamp;
+    }
+}
