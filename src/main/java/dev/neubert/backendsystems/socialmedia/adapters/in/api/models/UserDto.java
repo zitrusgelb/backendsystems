1adf0ab53ee45e50c3cc5fdfc581a47a4584e046
Adrian Stelter <gitmac@zitrusgelb.com>
2024-12-16 17:23:33 +0100

    feat: UserDTO

---
+++ src/main/java/dev/neubert/backendsystems/socialmedia/adapters/in/api/models/UserDto.java
@@ -0,0 +1,47 @@
+package dev.neubert.backendsystems.socialmedia.adapters.in.api.models;
+
+import java.util.List;
+
+public class UserDto extends AbstractDataTransferObject {
+    private long id;
+
+    private String username; // This is the CN from the THWS Auth
+    private String displayName;
+
+    private List<PostDto> posts;
+    private List<LikeDto> likes;
+
+    public UserDto() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public List<PostDto> getPosts() {
+        return posts;
+    }
+
+    public List<LikeDto> getLikes() {
+        return likes;
+    }
+}
