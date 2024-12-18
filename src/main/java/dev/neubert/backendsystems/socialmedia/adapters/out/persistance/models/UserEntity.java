fae184e141c7a17f76b8357ada658090a7d3cb72
Tim <tim.knuettel@study.thws.de>
2024-12-18 16:26:51 +0100

    fix: UserEntity.internalId -> UserEntity.id

--- src/main/java/dev/neubert/backendsystems/socialmedia/adapters/out/persistance/models/UserEntity.java
+++ src/main/java/dev/neubert/backendsystems/socialmedia/adapters/out/persistance/models/UserEntity.java
@@ -5,9 +5,10 @@ import jakarta.persistence.*;
 import java.util.List;
 
 @Entity
-public class UserEntity {
+public class UserEntity
+{
     @Id
-    private long internalId;
+    private long id;
 
     @Column(unique = true)
     private String username; // This is the CN from the THWS Auth
@@ -19,37 +20,47 @@ public class UserEntity {
     @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
     private List<LikeEntity> likes;
 
-    public UserEntity() {}
-
-    public long getInternalId() {
-        return internalId;
+    public UserEntity()
+    {
     }
 
-    public void setInternalId(long internalId) {
-        this.internalId = internalId;
+    public long getId()
+    {
+        return id;
     }
 
-    public String getUsername() {
+    public void setId(long id)
+    {
+        this.id = id;
+    }
+
+    public String getUsername()
+    {
         return username;
     }
 
-    public void setUsername(String username) {
+    public void setUsername(String username)
+    {
         this.username = username;
     }
 
-    public String getDisplayName() {
+    public String getDisplayName()
+    {
         return displayName;
     }
 
-    public void setDisplayName(String displayName) {
+    public void setDisplayName(String displayName)
+    {
         this.displayName = displayName;
     }
 
-    public List<PostEntity> getPosts() {
+    public List<PostEntity> getPosts()
+    {
         return posts;
     }
 
-    public List<LikeEntity> getLikes() {
+    public List<LikeEntity> getLikes()
+    {
         return likes;
     }
 }
