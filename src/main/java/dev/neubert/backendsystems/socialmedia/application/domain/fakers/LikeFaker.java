761d136eb1ff664b58d0848017e2f176df0c5e93
Laurin <laurin.neubert@ln13.de>
2024-12-19 18:30:26 +0100

    feat: created faker for Like

--- src/main/java/dev/neubert/backendsystems/socialmedia/application/domain/fakers/LikeFaker.java
+++ src/main/java/dev/neubert/backendsystems/socialmedia/application/domain/fakers/LikeFaker.java
@@ -2,10 +2,19 @@ package dev.neubert.backendsystems.socialmedia.application.domain.fakers;
 
 import dev.neubert.backendsystems.socialmedia.application.domain.models.Like;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
 public class LikeFaker extends AbstractFaker implements FakerMethods<Like> {
     @Override
     public Like createModel() {
-        // TODO: create actual likes
-        return null;
+        Like like = new Like();
+        Date date = faker.date().past(365, java.util.concurrent.TimeUnit.DAYS);
+        LocalDateTime createdAt = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+        like.setTimestamp(createdAt);
+        like.setUser(new UserFaker().createModel());
+        like.setPost(new PostFaker().createModel());
+        return like;
     }
 }
