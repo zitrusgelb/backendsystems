548e94d1b38d0b9d9b4b679e2d6e7a2141164fa6
Tim <tim.knuettel@study.thws.de>
2025-01-11 20:03:49 +0100

    feat: likeService

---
+++ src/main/java/dev/neubert/backendsystems/socialmedia/application/domain/services/LikeService.java
@@ -0,0 +1,45 @@
+package dev.neubert.backendsystems.socialmedia.application.domain.services;
+
+import dev.neubert.backendsystems.socialmedia.adapters.out.persistance.repository.LikeRepository;
+import dev.neubert.backendsystems.socialmedia.application.domain.models.Like;
+import dev.neubert.backendsystems.socialmedia.application.domain.models.Post;
+import dev.neubert.backendsystems.socialmedia.application.domain.models.User;
+import dev.neubert.backendsystems.socialmedia.application.port.in.Like.CreateLikeIn;
+import dev.neubert.backendsystems.socialmedia.application.port.in.Like.DeleteLikeIn;
+import dev.neubert.backendsystems.socialmedia.application.port.in.Like.ReadLikeByPostIn;
+import dev.neubert.backendsystems.socialmedia.application.port.in.Like.ReadLikeByUserIn;
+import jakarta.inject.Inject;
+
+import java.util.List;
+
+public class LikeService implements CreateLikeIn, DeleteLikeIn, ReadLikeByPostIn, ReadLikeByUserIn
+{
+    @Inject
+    private LikeRepository likeRepository;
+
+    @Override
+    public Like create(Like like)
+    {
+        //return likeRepository.createLike(like);
+        return null;
+    }
+
+    @Override
+    public boolean deleteLikeIn(Like like)
+    {
+        //return likeRepository.deleteLike(like.getId());
+        return false;
+    }
+
+    @Override
+    public List<Like> readLikeByPost(Post post)
+    {
+        return likeRepository.readLikeByPost(post);
+    }
+
+    @Override
+    public List<Like> readLikeByUser(User user)
+    {
+        return likeRepository.readLikeByUser(user);
+    }
+}
