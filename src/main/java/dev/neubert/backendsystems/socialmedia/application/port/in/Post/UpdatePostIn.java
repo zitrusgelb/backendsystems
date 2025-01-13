7cc71f9f13058f75823d555789681e38901fc067
Laurin <laurin.neubert@ln13.de>
2025-01-13 20:42:08 +0100

    fix: added id for the object to be updated

--- src/main/java/dev/neubert/backendsystems/socialmedia/application/port/in/Post/UpdatePostIn.java
+++ src/main/java/dev/neubert/backendsystems/socialmedia/application/port/in/Post/UpdatePostIn.java
@@ -3,5 +3,5 @@ package dev.neubert.backendsystems.socialmedia.application.port.in.Post;
 import dev.neubert.backendsystems.socialmedia.application.domain.models.Post;
 
 public interface UpdatePostIn {
-    Post updatePost(Post post);
+    Post updatePost(long postId, Post post);
 }
