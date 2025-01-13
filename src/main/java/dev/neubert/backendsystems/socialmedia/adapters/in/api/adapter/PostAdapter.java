cb1eef54c8483bea2ef9414fd3af99270ddca2b0
Laurin <laurin.neubert@ln13.de>
2025-01-13 19:53:04 +0100

    feat: added PostAdapter

---
+++ src/main/java/dev/neubert/backendsystems/socialmedia/adapters/in/api/adapter/PostAdapter.java
@@ -0,0 +1,54 @@
+package dev.neubert.backendsystems.socialmedia.adapters.in.api.adapter;
+
+import dev.neubert.backendsystems.socialmedia.adapters.in.api.models.PostDto;
+import dev.neubert.backendsystems.socialmedia.application.domain.mapper.PostMapper;
+import dev.neubert.backendsystems.socialmedia.application.domain.models.Post;
+import dev.neubert.backendsystems.socialmedia.application.port.in.Post.*;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@ApplicationScoped
+public class PostAdapter {
+    @Inject
+    CreatePostIn createPostIn;
+
+    @Inject
+    DeletePostIn deletePostIn;
+
+    @Inject
+    ReadAllPostsIn readAllPostsIn;
+
+    @Inject
+    ReadPostIn readPostIn;
+
+    @Inject
+    UpdatePostIn updatePostIn;
+
+    PostMapper postMapper = Mappers.getMapper(PostMapper.class);
+
+
+    public PostDto createPost(PostDto postDto) {
+        return postMapper.postToPostDto(createPostIn.create(postMapper.postDtoToPost(postDto)));
+    }
+
+    public boolean deletePost(PostDto postDto) {
+        return deletePostIn.delete(postMapper.postDtoToPost(postDto));
+    }
+
+    public List<Post> readAllPosts() {
+        return readAllPostsIn.readAllPosts();
+    }
+
+    public PostDto readPost(PostDto postDto) {
+        return postMapper.postToPostDto(readPostIn.readPost(postMapper.postDtoToPost(postDto)));
+    }
+
+    public PostDto updatePost(PostDto postDto){
+        return postMapper.postToPostDto(updatePostIn.updatePost(postMapper.postDtoToPost(postDto)));
+    }
+
+
+}
