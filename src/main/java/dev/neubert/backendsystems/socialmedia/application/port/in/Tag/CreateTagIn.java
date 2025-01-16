b6d90dd18ab4034648f3c96a6b2a106b2833f4b1
Jakob <@k65990>
2025-01-16 19:15:37 +0100

    fix: CreateTagIn, TagService

--- src/main/java/dev/neubert/backendsystems/socialmedia/application/port/in/Tag/CreateTagIn.java
+++ src/main/java/dev/neubert/backendsystems/socialmedia/application/port/in/Tag/CreateTagIn.java
@@ -3,5 +3,5 @@ package dev.neubert.backendsystems.socialmedia.application.port.in.Tag;
 import dev.neubert.backendsystems.socialmedia.application.domain.models.Tag;
 
 public interface CreateTagIn {
-    Tag createTag(Tag tag); // Ensure this matches the method in TagService
+    Tag createTag(Tag tag);
 }
