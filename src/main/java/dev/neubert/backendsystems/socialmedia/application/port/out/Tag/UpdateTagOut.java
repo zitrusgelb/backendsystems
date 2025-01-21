2f1f777e12ee5963fe8fe2b824b4bf97f481b2cf
Jakob <@k65990>
2025-01-21 15:12:39 +0100

    fix: getTagById, removed import statement

--- src/main/java/dev/neubert/backendsystems/socialmedia/application/port/out/Tag/UpdateTagOut.java
+++ src/main/java/dev/neubert/backendsystems/socialmedia/application/port/out/Tag/UpdateTagOut.java
@@ -1,7 +1,6 @@
 package dev.neubert.backendsystems.socialmedia.application.port.out.Tag;
 
 import dev.neubert.backendsystems.socialmedia.application.domain.models.Tag;
-import org.jboss.resteasy.util.NoContent;
 
 public interface UpdateTagOut {
     Tag updateTag(long id, Tag tag);
