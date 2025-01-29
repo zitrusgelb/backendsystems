0ab5da194be5decd851402fe8adb26599b539766
Tim <tim.knuettel@study.thws.de>
2025-01-29 11:18:39 +0100

    feat: LikeEntityId equals and hashCode

--- src/main/java/dev/neubert/backendsystems/socialmedia/adapters/out/persistance/models/LikeEntityId.java
+++ src/main/java/dev/neubert/backendsystems/socialmedia/adapters/out/persistance/models/LikeEntityId.java
@@ -1,14 +1,38 @@
 package dev.neubert.backendsystems.socialmedia.adapters.out.persistance.models;
 
 import java.io.Serializable;
+import java.util.Objects;
 
-public class LikeEntityId implements Serializable
-{
+public class LikeEntityId implements Serializable {
     private PostEntity post;
     private UserEntity user;
 
-    public LikeEntityId(PostEntity post, UserEntity user){
+    public LikeEntityId(PostEntity post, UserEntity user) {
         this.post = post;
         this.user = user;
+    }
+
+    public LikeEntityId() {}
+
+    public PostEntity getPost() {
+        return post;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LikeEntityId that = (LikeEntityId) o;
+        return Objects.equals(post.getId(), that.post.getId()) &&
+               Objects.equals(user.getId(), that.user.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(post.getId(), user.getId());
     }
 }
