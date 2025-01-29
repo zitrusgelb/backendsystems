1581941e646a05c5317da036e778a22fca246643
Tim <tim.knuettel@study.thws.de>
2025-01-29 18:55:13 +0100

    fix: double Usernames

--- src/main/java/dev/neubert/backendsystems/socialmedia/application/domain/fakers/UserFaker.java
+++ src/main/java/dev/neubert/backendsystems/socialmedia/application/domain/fakers/UserFaker.java
@@ -1,13 +1,22 @@
 package dev.neubert.backendsystems.socialmedia.application.domain.fakers;
 
+import dev.neubert.backendsystems.socialmedia.adapters.out.persistance.repository.UserRepository;
 import dev.neubert.backendsystems.socialmedia.application.domain.models.User;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 
 @ApplicationScoped
 public class UserFaker extends AbstractFaker implements FakerMethods<User> {
+    @Inject
+    UserRepository userRepository;
+
     @Override
     public User createModel() {
         String username = faker.pokemon().name();
+        if (userRepository.getUser(username) != null) {
+            username = username + "_";
+        }
+
         String displayName = faker.artist().name();
 
         return new User(username, displayName);
