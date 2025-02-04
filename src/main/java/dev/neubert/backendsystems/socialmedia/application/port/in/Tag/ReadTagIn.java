bdc1da837e58ea408c1c0a528d934eaea7b6163f
Jakob <@k65990>
2025-02-04 09:35:16 +0100

    fix: Replaced ReadTagByIdIn.java with ReadTagIn

--- src/main/java/dev/neubert/backendsystems/socialmedia/application/port/in/Tag/ReadTagIn.java
+++ src/main/java/dev/neubert/backendsystems/socialmedia/application/port/in/Tag/ReadTagIn.java
@@ -1,8 +1,7 @@
 package dev.neubert.backendsystems.socialmedia.application.port.in.Tag;
 
 import dev.neubert.backendsystems.socialmedia.application.domain.models.Tag;
-import java.util.List;
 
-public interface ReadTagByIdIn {
-    Tag readTagById(long id);
+public interface ReadTagIn {
+    Tag getTagById(long id);
 }
