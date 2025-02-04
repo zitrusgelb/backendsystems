e2b70e69777cfb9d6ff420c2d3466004bb399174
Jakob <@k65990>
2025-02-04 09:37:21 +0100

    fix: TagMapper

--- src/main/java/dev/neubert/backendsystems/socialmedia/application/domain/mapper/TagMapper.java
+++ src/main/java/dev/neubert/backendsystems/socialmedia/application/domain/mapper/TagMapper.java
@@ -8,13 +8,13 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(componentModel = "jakarta-cdi", uses = {PostMapper.class})
+@Mapper(componentModel = "jakarta-cdi",uses = {PostMapper.class})
 public interface TagMapper {
 
     @Mapping(target = "name", source = "name")
     TagDto tagToTagDto(Tag tag);
 
-
+    @Mapping(target = "name", source = "name")
     Tag tagDtoToTag(TagDto tagDto);
 
     TagEntity tagToTagEntity(Tag tag);
