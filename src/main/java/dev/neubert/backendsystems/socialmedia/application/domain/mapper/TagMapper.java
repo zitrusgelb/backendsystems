d380e8a6e2fba3598ab0e674d28b5576d046a5a8
Jakob <@k65990>
2025-02-04 09:37:21 +0100

    fix: TagMapper

--- src/main/java/dev/neubert/backendsystems/socialmedia/application/domain/mapper/TagMapper.java
+++ src/main/java/dev/neubert/backendsystems/socialmedia/application/domain/mapper/TagMapper.java
@@ -1,21 +1,27 @@
 package dev.neubert.backendsystems.socialmedia.application.domain.mapper;
 
+import dev.neubert.backendsystems.socialmedia.adapters.in.api.models.CreateTagDto;
 import dev.neubert.backendsystems.socialmedia.adapters.in.api.models.TagDto;
 import dev.neubert.backendsystems.socialmedia.adapters.out.persistance.models.TagEntity;
 import dev.neubert.backendsystems.socialmedia.application.domain.models.Tag;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(uses = {PostMapper.class})
-public interface TagMapper
-{
+public interface TagMapper {
     TagMapper INSTANCE = Mappers.getMapper(TagMapper.class);
 
+    @Mapping(target = "name", source = "name")
     TagDto tagToTagDto(Tag tag);
 
-    Tag tagDtoToTag(TagDto tag);
+    @Mapping(target = "name", source = "name")
+    Tag tagDtoToTag(TagDto tagDto);
 
     TagEntity tagToTagEntity(Tag tag);
 
-    Tag tagEntityToTag(TagEntity tag);
+    Tag tagEntityToTag(TagEntity tagEntity);
+
+    @Mapping(target = "name", source = "name")
+    Tag createTagDtoToTag(CreateTagDto createTagDto);
 }
