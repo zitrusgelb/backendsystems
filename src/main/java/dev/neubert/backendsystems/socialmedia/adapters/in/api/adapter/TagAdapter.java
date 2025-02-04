55745c9bf84fab4555a8309ff11e6ef5d20b1996
Jakob <@k65990>
2025-02-04 09:32:23 +0100

    fix: added createTagDto, added TagWebController, corrected CreateTagIn, TagService, TagDto

--- src/main/java/dev/neubert/backendsystems/socialmedia/adapters/in/api/adapter/TagAdapter.java
+++ src/main/java/dev/neubert/backendsystems/socialmedia/adapters/in/api/adapter/TagAdapter.java
@@ -1,22 +1,19 @@
 package dev.neubert.backendsystems.socialmedia.adapters.in.api.adapter;
 
+import dev.neubert.backendsystems.socialmedia.adapters.in.api.models.CreateTagDto;
 import dev.neubert.backendsystems.socialmedia.adapters.in.api.models.TagDto;
-import dev.neubert.backendsystems.socialmedia.adapters.out.persistance.repository.TagRepository;
 import dev.neubert.backendsystems.socialmedia.application.domain.mapper.TagMapper;
 import dev.neubert.backendsystems.socialmedia.application.domain.models.Tag;
-import dev.neubert.backendsystems.socialmedia.application.port.in.Tag.CreateTagIn;
-import dev.neubert.backendsystems.socialmedia.application.port.in.Tag.DeleteTagIn;
-import dev.neubert.backendsystems.socialmedia.application.port.in.Tag.ReadAllTagsIn;
-import dev.neubert.backendsystems.socialmedia.application.port.in.Tag.ReadTagByIdIn;
-import dev.neubert.backendsystems.socialmedia.application.port.in.Tag.UpdateTagIn;
+import dev.neubert.backendsystems.socialmedia.application.port.in.Tag.*;
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@ApplicationScoped
 public class TagAdapter {
-
     @Inject
     CreateTagIn createTagIn;
 
@@ -27,34 +24,34 @@ public class TagAdapter {
     ReadAllTagsIn readAllTagsIn;
 
     @Inject
-    ReadTagByIdIn readTagByIdIn;
+    ReadTagIn readTagIn;
 
     @Inject
     UpdateTagIn updateTagIn;
 
     TagMapper tagMapper = Mappers.getMapper(TagMapper.class);
 
-    public TagDto createTag(TagDto tagDto) {
-        return tagMapper.tagToTagDto(createTagIn.createTag(tagMapper.tagDtoToTag(tagDto)));
+    public TagDto createTag(CreateTagDto tagDto) {
+        return tagMapper.tagToTagDto(createTagIn.create(tagMapper.createTagDtoToTag(tagDto)));
     }
 
     public boolean deleteTag(TagDto tagDto) {
-        return deleteTagIn.deleteTag(tagMapper.tagDtoToTag(tagDto));
+        Tag tag = tagMapper.tagDtoToTag(tagDto);
+        return deleteTagIn.delete(tag);
     }
 
-    public List<TagDto> getAllTags() {
+    public List<TagDto> readAllTags() {
         List<Tag> tags = readAllTagsIn.readAllTags();
         return tags.stream().map(tagMapper::tagToTagDto).collect(Collectors.toList());
     }
 
-
-    public TagDto updateTag(TagDto tagDto) {
-        Tag updatedTag = updateTagIn.updateTag(tagDto.getId(), tagMapper.tagDtoToTag(tagDto));
-        return tagMapper.tagToTagDto(updatedTag);
-    }
     public TagDto getTagById(long id) {
-        Tag tag = readTagByIdIn.readTagById(id);
+        Tag tag = readTagIn.getTagById(id);
         return tagMapper.tagToTagDto(tag);
     }
 
+    public TagDto updateTag(long id, TagDto tagDto) {
+        return tagMapper.tagToTagDto(updateTagIn.update(id, tagMapper.tagDtoToTag(tagDto)));
+
+    }
 }
