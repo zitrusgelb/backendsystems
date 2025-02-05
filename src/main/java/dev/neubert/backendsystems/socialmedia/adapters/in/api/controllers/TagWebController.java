b1bb7e764acfc2f704d114ade28df4305a742f2b
Jakob <@k65990>
2025-02-05 09:10:43 +0100

    fix: removed TagAdapter.java, updated TagWebController

--- src/main/java/dev/neubert/backendsystems/socialmedia/adapters/in/api/controllers/TagWebController.java
+++ src/main/java/dev/neubert/backendsystems/socialmedia/adapters/in/api/controllers/TagWebController.java
@@ -1,22 +1,40 @@
 package dev.neubert.backendsystems.socialmedia.adapters.in.api.controllers;
 
-import dev.neubert.backendsystems.socialmedia.adapters.in.api.adapter.TagAdapter;
 import dev.neubert.backendsystems.socialmedia.adapters.in.api.models.CreateTagDto;
 import dev.neubert.backendsystems.socialmedia.adapters.in.api.models.TagDto;
-import io.netty.handler.codec.http.HttpResponseStatus;
+import dev.neubert.backendsystems.socialmedia.application.domain.mapper.TagMapper;
+import dev.neubert.backendsystems.socialmedia.application.domain.models.Tag;
+import dev.neubert.backendsystems.socialmedia.application.port.in.Tag.*;
 import jakarta.inject.Inject;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.*;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Path("tags")
 public class TagWebController {
 
     @Inject
-    TagAdapter tagAdapter;
+    CreateTagIn createTagIn;
+
+    @Inject
+    DeleteTagIn deleteTagIn;
+
+    @Inject
+    ReadAllTagsIn readAllTagsIn;
+
+    @Inject
+    ReadTagIn readTagIn;
+
+    @Inject
+    UpdateTagIn updateTagIn;
+
+    TagMapper tagMapper = Mappers.getMapper(TagMapper.class);
 
     @Context
     private UriInfo uriInfo;
@@ -27,86 +45,50 @@ public class TagWebController {
     @Produces({MediaType.APPLICATION_JSON})
     public Response readAllTags(
             @DefaultValue("") @QueryParam("q") String query,
-            @PositiveOrZero @DefaultValue("0") @QueryParam("offset") long offset,
-            @Positive @DefaultValue("20") @QueryParam("size") long size
-    ) {
-        setCacheControlFiveMinutes();
-        var allTags = this.tagAdapter.readAllTags();
-        var filteredTags = allTags.stream().filter(tag -> tag.getName().contains(query)).toList();
-        var result = filteredTags.stream().skip(offset).limit(size).collect(Collectors.toList());
-        return Response.status(HttpResponseStatus.OK.code())
-                       .header("X-Total-Count", result.size())
-                       .cacheControl(this.cacheControl)
-                       .entity(result)
-                       .build();
-    }
-
-    @Path("{id}")
-    @GET
-    @Produces({MediaType.APPLICATION_JSON})
-    public Response getById(@Positive @PathParam("id") long id) {
-        setCacheControlFiveMinutes();
-        var requestedTag = this.tagAdapter.getTagById(id);
-        if (requestedTag == null) {
-            return Response.status(HttpResponseStatus.NOT_FOUND.code()).build();
-        }
-        return Response.ok(requestedTag)
-                       .tag(Long.toString(requestedTag.hashCode()))
-                       .cacheControl(this.cacheControl)
-                       .build();
+            @PositiveOrZero @DefaultValue("0") @QueryParam("offset") int offset,
+            @Positive @DefaultValue("10") @QueryParam("limit") int limit) {
+        List<Tag> tags = readAllTagsIn.readAllTags();
+        List<TagDto> tagDtos = tags.stream().map(tagMapper::tagToTagDto).collect(Collectors.toList());
+        return Response.ok(tagDtos).build();
     }
 
     @POST
     @Consumes({MediaType.APPLICATION_JSON})
-    public Response createTag(
-            @Valid
-            CreateTagDto model
-    ) {
-        setCacheControlFiveMinutes();
-        var result = this.tagAdapter.createTag(model);
-        return Response.status(HttpResponseStatus.CREATED.code())
-                       .header("Location", createLocationHeader(result))
-                       .tag(Long.toString(result.hashCode()))
-                       .cacheControl(this.cacheControl)
-                       .build();
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response createTag(@Valid CreateTagDto tagDto) {
+        TagDto createdTag = tagMapper.tagToTagDto(createTagIn.createTag(tagMapper.createTagDtoToTag(tagDto)));
+        UriBuilder builder = uriInfo.getAbsolutePathBuilder();
+        builder.path(Long.toString(createdTag.getId()));
+        return Response.created(builder.build()).entity(createdTag).build();
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response getTagById(@PathParam("id") long id) {
+        Tag tag = readTagIn.getTagById(id);
+        return Response.ok(tagMapper.tagToTagDto(tag)).build();
     }
 
     @PUT
     @Path("{id}")
     @Consumes({MediaType.APPLICATION_JSON})
-    public Response updateTag(@Positive @PathParam("id") long id, @Valid TagDto model) {
-        setCacheControlFiveMinutes();
-        if (this.tagAdapter.getTagById(id) == null) {
-            return Response.status(HttpResponseStatus.NOT_FOUND.code()).build();
-        }
-        var result = this.tagAdapter.updateTag(id, model);
-        return Response.status(HttpResponseStatus.NO_CONTENT.code())
-                       .header("Location", createLocationHeader(result))
-                       .tag(Long.toString(result.hashCode()))
-                       .cacheControl(this.cacheControl)
-                       .build();
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response updateTag(@PathParam("id") long id, @Valid TagDto tagDto) {
+        TagDto updatedTag = tagMapper.tagToTagDto(updateTagIn.update(id, tagMapper.tagDtoToTag(tagDto)));
+        return Response.ok(updatedTag).build();
     }
 
     @DELETE
     @Path("{id}")
-    public Response deleteTag(@Positive @PathParam("id") long id) {
-        var toBeDeleted = this.tagAdapter.getTagById(id);
-        if (toBeDeleted == null) {
-            return Response.status(HttpResponseStatus.NOT_FOUND.code()).build();
-        }
-        if (this.tagAdapter.deleteTag(toBeDeleted)) {
-            return Response.status(HttpResponseStatus.NO_CONTENT.code()).build();
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response deleteTag(@PathParam("id") long id) {
+        Tag tag = readTagIn.getTagById(id);
+        boolean deleted = deleteTagIn.deleteTag(tag);
+        if (deleted) {
+            return Response.noContent().build();
         } else {
-            return Response.status(HttpResponseStatus.INTERNAL_SERVER_ERROR.code()).build();
+            return Response.status(Response.Status.NOT_FOUND).build();
         }
-    }
-
-    private String createLocationHeader(TagDto model) {
-        return uriInfo.getRequestUriBuilder().path(Long.toString(model.getId())).build().toString();
-    }
-
-    private void setCacheControlFiveMinutes() {
-        this.cacheControl = new CacheControl();
-        this.cacheControl.setMaxAge(300);
     }
 }
