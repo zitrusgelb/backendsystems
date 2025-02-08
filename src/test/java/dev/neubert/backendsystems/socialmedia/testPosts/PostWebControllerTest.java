afd7acc661597e4716cf982f52b38de86fe48cf6
Laurin <laurin.neubert@ln13.de>
2025-02-08 09:31:38 +0100

    test: added more tests for deleting and updating posts

--- src/test/java/dev/neubert/backendsystems/socialmedia/testPosts/PostWebControllerTest.java
+++ src/test/java/dev/neubert/backendsystems/socialmedia/testPosts/PostWebControllerTest.java
@@ -53,7 +53,7 @@ public class PostWebControllerTest {
         String postResponseHeaders = given().contentType(ContentType.JSON)
                                             .body("""
                                                   {
-                                                          "content": "I am your father",
+                                                          "content": "This is where the fun begins.",
                                                           "tag": null,
                                                           "replyTo": null
                                                       }
@@ -73,6 +73,40 @@ public class PostWebControllerTest {
                .statusCode(200);
 
     }
+
+    @Test
+    void testDeletePost() {
+        String postResponseHeaders =
+                given().contentType(ContentType.JSON)
+                       .header("X-User-Id", 1)
+                       .body("""
+                             {
+                                     "content": "I am your father",
+                                     "tag": null,
+                                     "replyTo": null
+                                 }
+                             """)
+                       .when()
+                       .post("/posts")
+                       .headers()
+                       .toString();
+        Matcher locationMatcher = fullLocationPattern.matcher(postResponseHeaders);
+        String location = locationMatcher.find() ? locationMatcher.group() : null;
+
+        String postToBeDeleted =
+                given().contentType(ContentType.JSON).when().get(location).getBody().asString();
+
+        given().contentType(ContentType.JSON)
+               .header("X-User-Id", 1)
+               .header("If-Match", "v1")
+               .body(postToBeDeleted)
+               .when()
+               .delete(location)
+               .then()
+               .assertThat()
+               .statusCode(204);
+    }
+
 
     @Test
     void testDeletePostWrongUser() {
@@ -101,7 +135,117 @@ public class PostWebControllerTest {
                .then()
                .assertThat()
                .header("content-length", "0")
-               .statusCode(401); // TODO: fix 401
+               .statusCode(401);
+    }
+
+    @Test
+    void testPutPost() {
+        String postResponseHeaders =
+                given().contentType(ContentType.JSON)
+                       .header("X-User-Id", 1)
+                       .body("""
+                             {
+                                     "content": "Hello there!",
+                                     "tag": null,
+                                     "replyTo": null
+                                 }
+                             """)
+                       .when()
+                       .post("/posts")
+                       .headers()
+                       .toString();
+        Matcher locationMatcher = fullLocationPattern.matcher(postResponseHeaders);
+        String location = locationMatcher.find() ? locationMatcher.group() : null;
+
+        String postToBeUpdated = given().contentType(ContentType.JSON)
+                                        .when()
+                                        .get(location)
+                                        .getBody()
+                                        .asString()
+                                        .replace("\"Hello there!\"",
+                                                 "\"General Kenobi, you are a bold one!\"");
+
+        given().contentType(ContentType.JSON)
+               .header("X-User-Id", 1)
+               .header("If-Match", "v1")
+               .body(postToBeUpdated)
+               .when()
+               .put(location)
+               .then()
+               .assertThat()
+               .header("Cache-Control", "no-transform, max-age=300")
+               .statusCode(204);
+    }
+
+    @Test
+    void testPutFailPreconditions() {
+        String postResponseHeaders =
+                given().contentType(ContentType.JSON)
+                       .header("X-User-Id", 1)
+                       .body("""
+                             {
+                                     "content": "Hello there!",
+                                     "tag": null,
+                                     "replyTo": null
+                                 }
+                             """)
+                       .when()
+                       .post("/posts")
+                       .headers()
+                       .toString();
+        Matcher locationMatcher = fullLocationPattern.matcher(postResponseHeaders);
+        String location = locationMatcher.find() ? locationMatcher.group() : null;
+
+        String postToBeUpdated = given().contentType(ContentType.JSON)
+                                        .when()
+                                        .get(location)
+                                        .getBody()
+                                        .asString()
+                                        .replace("\"Hello there!\"",
+                                                 "\"General Kenobi, you are a bold one!\"");
+
+        given().contentType(ContentType.JSON)
+               .header("X-User-Id", 1)
+               .body(postToBeUpdated)
+               .when()
+               .put(location)
+               .then()
+               .assertThat()
+               .header("content-length", "0")
+               .statusCode(412);
+    }
+
+    @Test
+    void testPutPostWrongUser() {
+        String postResponseHeaders = given().contentType(ContentType.JSON)
+                                            .body("""
+                                                  {
+                                                          "content": "I am your father",
+                                                          "tag": null,
+                                                          "replyTo": null
+                                                      }
+                                                  """)
+                                            .when()
+                                            .post("/posts")
+                                            .headers()
+                                            .toString();
+        Matcher locationMatcher = fullLocationPattern.matcher(postResponseHeaders);
+        String location = locationMatcher.find() ? locationMatcher.group() : null;
+
+        String postToBeUpdated = given().contentType(ContentType.JSON)
+                                        .when()
+                                        .get(location)
+                                        .getBody()
+                                        .asString()
+                                        .replace("\"I am your father\"", "\"Noooooooooooooo!\"");
+        given().contentType(ContentType.JSON)
+               .body(postToBeUpdated)
+               .when()
+               .put(location)
+               .then()
+               .assertThat()
+               .header("content-length", "0")
+               .statusCode(401);
     }
 
     @Test
@@ -132,7 +276,7 @@ public class PostWebControllerTest {
                .put("/posts/9999999999999")
                .then()
                .assertThat()
-               .statusCode(400);
+               .statusCode(404);
     }
 
     @Test
