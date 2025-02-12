8962e422d4e1ffc83c91faf6cb06783d10a9cd19
Jakob <@k65990>
2025-02-12 14:27:14 +0100

    feat: added errorhandeling to TagWebController added more Tests

--- src/test/java/dev/neubert/backendsystems/socialmedia/testTags/TagWebControllerTest.java
+++ src/test/java/dev/neubert/backendsystems/socialmedia/testTags/TagWebControllerTest.java
@@ -1,8 +1,8 @@
 package dev.neubert.backendsystems.socialmedia.testTags;
 
 import dev.neubert.backendsystems.socialmedia.adapters.in.api.models.CreateTagDto;
+import dev.neubert.backendsystems.socialmedia.adapters.in.api.models.TagDto;
 import dev.neubert.backendsystems.socialmedia.application.domain.fakers.TagFaker;
-import dev.neubert.backendsystems.socialmedia.application.domain.mapper.TagMapper;
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
@@ -16,22 +16,19 @@ public class TagWebControllerTest {
     @Inject
     TagFaker tagFaker;
 
-    @Inject
-    TagMapper tagMapper;
-
     @Test
     void getAllTagsNoTagsExisting() {
         given().when()
                .get("/tags")
                .then()
-               .statusCode(200)
-               .body(is("[]"));
+               .statusCode(204);
     }
 
     @Test
     void createTag() {
         var tag = tagFaker.createModel();
-        CreateTagDto createTagDto = tagMapper.tagToCreateTagDto(tag);
+        CreateTagDto createTagDto = new CreateTagDto();
+        createTagDto.setName(tag.getName());
 
         given().contentType("application/json")
                .body(createTagDto)
@@ -40,6 +37,52 @@ public class TagWebControllerTest {
                .then()
                .statusCode(201)
                .header("Location", containsString("/tags/"));
+    }
 
+    @Test
+    void createTagWithEmptyNameShouldReturnBadRequest() {
+        CreateTagDto createTagDto = new CreateTagDto();
+        createTagDto.setName("");
+
+        given().contentType("application/json")
+               .body(createTagDto)
+               .when()
+               .post("/tags")
+               .then()
+               .statusCode(400)
+               .body(equalTo("Tag-Name darf nicht leer sein"));
+    }
+
+    @Test
+    void getNonExistingTagShouldReturnNotFound() {
+        given().when()
+               .get("/tags/9999")
+               .then()
+               .statusCode(404)
+               .body(equalTo("Tag nicht gefunden"));
+    }
+
+    @Test
+    void deleteNonExistingTagShouldReturnNotFound() {
+        given().when()
+               .delete("/tags/9999")
+               .then()
+               .statusCode(404)
+               .body(equalTo("Tag nicht gefunden"));
+    }
+
+    @Test
+    void updateNonExistingTagShouldReturnNotFound() {
+        TagDto tagDto = new TagDto();
+        tagDto.setId(9999L);
+        tagDto.setName("Updated Tag");
+
+        given().contentType("application/json")
+               .body(tagDto)
+               .when()
+               .put("/tags/9999")
+               .then()
+               .statusCode(404)
+               .body(equalTo("Tag nicht gefunden"));
     }
 }
