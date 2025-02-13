3c5054f38223a7b6666b9dc230e5e879fa02d3c7
Tim <tim.knuettel@study.thws.de>
2025-02-13 17:40:28 +0100

    refactor: CreateTagDto

--- src/main/java/dev/neubert/backendsystems/socialmedia/application/domain/mapper/TagMapper.java
+++ src/main/java/dev/neubert/backendsystems/socialmedia/application/domain/mapper/TagMapper.java
@@ -1,11 +1,9 @@
 package dev.neubert.backendsystems.socialmedia.application.domain.mapper;
 
-import dev.neubert.backendsystems.socialmedia.adapters.in.api.models.CreateTagDto;
 import dev.neubert.backendsystems.socialmedia.adapters.in.api.models.TagDto;
 import dev.neubert.backendsystems.socialmedia.adapters.out.persistance.models.TagEntity;
 import dev.neubert.backendsystems.socialmedia.application.domain.models.Tag;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "jakarta-cdi", uses = {PostMapper.class})
 public interface TagMapper {
@@ -17,8 +15,4 @@ public interface TagMapper {
     TagEntity tagToTagEntity(Tag tag);
 
     Tag tagEntityToTag(TagEntity tag);
-
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "posts", ignore = true)
-    Tag createTagDtoToTag(CreateTagDto createTagDto);
 }
