c8b94e58e886eac1589bfc5f85d4e8f02e3e874e
Laurin <laurin.neubert@ln13.de>
2025-02-15 13:33:10 +0100

    test: added more tests for User

--- src/test/java/dev/neubert/backendsystems/socialmedia/testUsers/UserWebControllerIT.java
+++ src/test/java/dev/neubert/backendsystems/socialmedia/testUsers/UserWebControllerIT.java
@@ -3,12 +3,15 @@ package dev.neubert.backendsystems.socialmedia.testUsers;
 import dev.neubert.backendsystems.socialmedia.adapters.in.api.models.UserDto;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 import static io.restassured.RestAssured.given;
+import static org.wildfly.common.Assert.assertTrue;
 
 @QuarkusIntegrationTest
 public class UserWebControllerIT {
@@ -68,5 +71,77 @@ public class UserWebControllerIT {
                           .as(UserDto.class);
 
         assert body.getUsername().equals(userName);
+    }
+
+    @Test
+    public void testGetNonExistentUser() {
+        given().when().get("/users/9999999999").then().assertThat().statusCode(404);
+    }
+
+    @Test
+    public void testGetUserInvalidUsername() {
+        given().when().get("/users/invalidUsername").then().assertThat().statusCode(404);
+    }
+
+    @Test
+    public void testGetPostsByUser() {
+
+
+        given().when()
+               .contentType(ContentType.JSON)
+               .header("X-Integration-Test", "true")
+               .header("X-Username", "R2-D2")
+               .body("""
+                     {
+                             "content": "These aren't the droids you are looking for.",
+                             "tagName":"Star Wars",
+                             "replyTo": null
+                         }
+                     """)
+               .post("/posts")
+               .then()
+               .assertThat()
+               .statusCode(201);
+
+        given().when()
+               .contentType(ContentType.JSON)
+               .header("X-Integration-Test", "true")
+               .header("X-Username", "R2-D2")
+               .body("""
+                     {
+                             "content": "I have a bad feeling about this...",
+                             "tagName":"Star Wars",
+                             "replyTo": null
+                         }
+                     """)
+               .post("/posts")
+               .then()
+               .assertThat()
+               .statusCode(201);
+
+        given().contentType(ContentType.JSON)
+               .when()
+               .header("X-Integration-Test", "true")
+               .header("X-Username", "R2-D2")
+               .get("/users/R2-D2")
+               .then()
+               .assertThat()
+               .statusCode(200);
+
+        var responseBody = given().contentType(ContentType.JSON)
+                                  .when()
+                                  .header("X-Integration-Test", "true")
+                                  .header("X-Username", "R2-D2")
+                                  .get("/users/R2-D2")
+                                  .getBody()
+                                  .asString();
+
+        Pattern content1 =
+                Pattern.compile("\"content\":\"These aren't the droids you are looking for.\"");
+        Pattern content2 = Pattern.compile("\"content\":\"I have a bad feeling about this...\"");
+
+        assertTrue(content1.matcher(responseBody).find());
+        assertTrue(content2.matcher(responseBody).find());
+
     }
 }
--- src/test/java/dev/neubert/backendsystems/socialmedia/testUsers/UserWebControllerTest.java
+++ src/test/java/dev/neubert/backendsystems/socialmedia/testUsers/UserWebControllerTest.java
@@ -4,12 +4,15 @@ import dev.neubert.backendsystems.socialmedia.adapters.in.api.models.UserDto;
 import dev.neubert.backendsystems.socialmedia.application.domain.fakers.UserFaker;
 import dev.neubert.backendsystems.socialmedia.application.port.in.User.CreateUserIn;
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 import static io.restassured.RestAssured.given;
+import static org.wildfly.common.Assert.assertTrue;
 
 @QuarkusTest
 public class UserWebControllerTest {
@@ -25,7 +28,8 @@ public class UserWebControllerTest {
         createUserIn.createUser(user);
 
         var body = given().when()
-                          .get("/users?limit=10")
+                          .queryParam("limit", "10")
+                          .get("/users")
                           .then()
                           .assertThat()
                           .statusCode(200)
@@ -71,4 +75,75 @@ public class UserWebControllerTest {
         assert user.getUsername().equals(body.getUsername());
         assert user.getDisplayName().equals(body.getDisplayName());
     }
+
+
+    @Test
+    public void testGetNonExistentUser() {
+        given().when().get("/users/9999999999").then().assertThat().statusCode(404);
+    }
+
+    @Test
+    public void testGetUserInvalidUsername() {
+        given().when().get("/users/invalidUsername").then().assertThat().statusCode(404);
+    }
+
+    @Test
+    public void testGetPostsByUser() {
+        var user = userFaker.createModel();
+        user = createUserIn.createUser(user);
+
+        given().when()
+               .contentType(ContentType.JSON)
+               .header("X-User-Id", user.getId())
+               .body("""
+                     {
+                             "content": "These aren't the droids you are looking for.",
+                             "tagName":"Star Wars",
+                             "replyTo": null
+                         }
+                     """)
+               .post("/posts")
+               .then()
+               .assertThat()
+               .statusCode(201);
+
+        given().when()
+               .contentType(ContentType.JSON)
+               .header("X-User-Id", user.getId())
+               .body("""
+                     {
+                             "content": "I have a bad feeling about this...",
+                             "tagName":"Star Wars",
+                             "replyTo": null
+                         }
+                     """)
+               .post("/posts")
+               .then()
+               .assertThat()
+               .statusCode(201);
+
+        given().contentType(ContentType.JSON)
+               .when()
+               .header("X-User-Id", user.getId())
+               .get("/users/" + user.getUsername())
+               .then()
+               .assertThat()
+               .statusCode(200);
+
+        var responseBody = given().contentType(ContentType.JSON)
+                                  .when()
+                                  .header("X-User-Id", user.getId())
+                                  .get("/users/" + user.getUsername())
+                                  .getBody()
+                                  .asString();
+
+        Pattern content1 =
+                Pattern.compile("\"content\":\"These aren't the droids you are looking for.\"");
+        Pattern content2 = Pattern.compile("\"content\":\"I have a bad feeling about this...\"");
+
+        assertTrue(content1.matcher(responseBody).find());
+        assertTrue(content2.matcher(responseBody).find());
+
+    }
+
 }
